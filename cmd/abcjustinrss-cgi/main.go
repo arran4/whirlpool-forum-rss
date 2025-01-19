db349aa0d23ba46621525f8cf6ab67bc5dd0eb91
Arran Ubels <arran4@gmail.com>
2025-01-19 17:32:21 +1100

    Cgi version for later.

---
+++ cmd/abcjustinrss-cgi/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"encoding/xml"
+	"fmt"
+	"github.com/arran4/abc-justin-rss"
+	"log"
+	"net/http"
+	"net/http/cgi"
+)
+
+func main() {
+	log.Fatal(cgi.Serve(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		err, rss := abcjustinrss.FetchAndParseNewsToRSS()
+		if err != nil {
+			http.Error(w, "Failed to fetch and parse RSS", http.StatusInternalServerError)
+			return
+		}
+
+		output, err := xml.MarshalIndent(rss, "", "  ")
+		if err != nil {
+			http.Error(w, "Failed to marshal RSS", http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/rss+xml")
+		_, _ = fmt.Fprintf(w, "%s%s", xml.Header, output)
+	})))
+}
